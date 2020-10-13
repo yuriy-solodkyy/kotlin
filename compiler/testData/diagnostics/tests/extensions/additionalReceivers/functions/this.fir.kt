3c45cc0b5a098727fd9ca50058249e246e07463a
Anastasiya Shadrina <a.shadrina5@mail.ru>
2020-10-13 19:21:36 +0700

    Change left multiple with syntax

--- compiler/testData/diagnostics/tests/extensions/additionalReceivers/functions/this.fir.kt
+++ compiler/testData/diagnostics/tests/extensions/additionalReceivers/functions/this.fir.kt
@@ -1,7 +1,10 @@
 class A<T>(val a: T)
 class B(val b: Any)
 
-with<A<Int>, A<String>, B> fun f() {
+with<A<Int>>
+with<A<String>>
+with<B>
+fun f() {
     <!UNRESOLVED_LABEL!>this@A<!>.<!UNRESOLVED_REFERENCE!>a<!>.<!UNRESOLVED_REFERENCE!>length<!>
     <!UNRESOLVED_LABEL!>this@B<!>.<!UNRESOLVED_REFERENCE!>b<!>
     <!NO_THIS!>this<!>
--- compiler/testData/diagnostics/tests/extensions/additionalReceivers/functions/this.kt
+++ compiler/testData/diagnostics/tests/extensions/additionalReceivers/functions/this.kt
@@ -1,7 +1,10 @@
 class A<T>(val a: T)
 class B(val b: Any)
 
-with<A<Int>, A<String>, B> fun f() {
+with<A<Int>>
+with<A<String>>
+with<B>
+fun f() {
     this@A.a.length
     this@B.b
     <!NO_THIS!>this<!>
